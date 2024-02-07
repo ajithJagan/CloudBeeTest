2bfd39392f87159f98db94fc0b73a61ced37c9bd
Ajith.j <ajith.j@qbrainx.com>
2024-02-07 15:57:40 +0530

    07-02-24

---
+++ src/test/java/com/api/poc/Controller/TicketControllerTest.java
@@ -0,0 +1,28 @@
+package com.api.poc.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketControllerTest {
+
+    @Test
+    void purchaseTicket() {
+    }
+
+    @Test
+    void getTicket() {
+    }
+
+    @Test
+    void getUsersBySection() {
+    }
+
+    @Test
+    void removeUser() {
+    }
+
+    @Test
+    void modifySeat() {
+    }
+}
