e0a7b646ddae87f7120ab0acf1bd7b7e859a5100
Ajith.j <ajith.j@qbrainx.com>
2024-02-08 17:14:36 +0530

    08-02-24

--- src/main/java/com/api/poc/PocApplication.java
+++ src/main/java/com/api/poc/PocApplication.java
@@ -1,17 +1,14 @@
 package com.api.poc;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class PocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocApplication.class, args);
 		System.out.println("Poc is running...");
 
-		log.info("");
 	}
 }
